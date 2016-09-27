7961cd6bca6e6173575c5ee0a6c901c998143515
ppker <yanzhipeng@jucaicat.com>
2016-09-27 17:49:17 +0800

    test

--- get_dianrong.php
+++ get_dianrong.php
@@ -9,4 +9,11 @@ require_once './function.php';
 require_once './spider/curl.php';
 
 $result = Curl::request('GET', 'https://www.dianrong.com/market');
+
+preg_match('#<span class="education item" title=["|\'](.*?)["|\']>#', $result, $out);
+
+preg_match('#<p class="title">\s(.*?)\s</span>#', $result, $out);
+
+var_dump($out);die;
+
 $re = push_log([$result]);
