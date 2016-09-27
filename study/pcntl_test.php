9fbc5ca71b8777f786d3d26f2a45753c4c92dc0e
ppker <yanzhipeng@jucaicat.com>
2016-09-27 15:25:39 +0800

    新增的测试多进程阻塞式

---
+++ study/pcntl_test.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: ZhiPeng
+ * Date: 2016/9/27
+ * Project: Cat Visual
+ */
+
+date_default_timezone_set("Asia/Shanghai");
+function beep($process_name) {
+
+    echo $process_name . " 我的进程PID是: " . getmypid() . " " . date("Y-m-d H:i:s") . " \n";
+    sleep(1);
+}
+
+echo "全局父进程开始, pid :" . getmypid() . " \n";
+
+for ($i = 0; $i < 3; ++$i) {
+    $pid = pcntl_fork();
+    if (-1 == $pid) {
+        die("cannot forn son proces");
+    } elseif (0 < $pid) {
+        echo "父进程 执行 \n";
+        for ($k = 0; $k < 2; ++$k) {
+            beep("父进程");
+        }
+    } elseif (0 == $pid) {
+        echo "子进程 执行 PID:" . getmypid() . " \n";
+        for ($j = 0; $j < 5; ++$j) {
+            beep("子进程" . getmypid());
+        }
+        exit; // 释放子进程
+    }
+}
